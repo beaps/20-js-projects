a7508788600b4e1bac19016e8e090b59c4dedf6a
Bea P. Santana <beapsant@gmail.com>
2020-09-22 13:38:38 +0100

    Add Picture in Picture

--- picture-in-picture/script.js
+++ picture-in-picture/script.js
@@ -1,1 +1,27 @@
-console.log('hola')
+const video = document.querySelector('#video');
+const button = document.querySelector('#button');
+
+// 1. Prompt to select media stream
+// 2. Pass to video element
+// 3. Play
+async function selectMediaStream() {
+  try {
+    // Screen Capture API
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = () => {
+      video.play();
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+button.addEventListener('click', async () => {
+  button.disabled = true;
+  // Start Picture in Picture
+  await video.requestPictureInPicture();
+  button.disabled = false;
+});
+
+selectMediaStream();
