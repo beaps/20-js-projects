be9172f3aa93767df97bdf6e727076e80d2b0deb
Bea P. Santana <beapsant@gmail.com>
2020-09-23 12:39:50 +0100

    Set Text to Speech API

--- joke-teller/script.js
+++ joke-teller/script.js
@@ -1,3 +1,54 @@
+const audioElement = document.querySelector('#audio');
+const button = document.querySelector('#button');
 
-// VoiceRSS Javascript SDK
-const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
+// Get jokes from Joke API
+async function getJoke() {
+  const API_URL =
+    'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+  try {
+    const response = await fetch(API_URL);
+    let data = await response.json();
+    let joke = selectJoke(data);
+    return joke;
+  } catch (error) {
+    console.log(error);
+    return {};
+  }
+}
+
+// There are 2 types of jokes: twopart and single
+// and because of that the data object has different properties
+function selectJoke(data) {
+  if (data.type === 'twopart') {
+    return `${data.setup} ... ${data.delivery}`;
+  } else {
+    return data.joke;
+  }
+}
+
+// Pass jokes to Text-to-Speech (Voice RSS API)
+async function tellMe(fn) {
+  try {
+    VoiceRSS.speech({
+      key: '690c091f9dae442f98f3ca693327375f',
+      src: await fn(),
+      hl: 'en-us',
+      v: 'Linda',
+      r: 0,
+      c: 'mp3',
+      f: '44khz_16bit_stereo',
+      ssml: false
+    });
+    // Disable button
+    toggleButton();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+button.addEventListener('click', () => tellMe(getJoke));
+audioElement.addEventListener('ended', toggleButton);
