c06b5a6faf585161cd91d09c59070e31dc410896
Bea P. Santana <beapsant@gmail.com>
2020-09-24 18:15:51 +0100

    Add dark mode

--- dark-mode/script.js
+++ dark-mode/script.js
@@ -1,5 +1,40 @@
 const input = document.querySelector('#switch__input');
+const nav = document.querySelector('#nav');
+const themeText = document.querySelector('.theme__text');
+const themeIcon = document.querySelector('.theme__icon');
+const image1 = document.querySelector('#image1');
+const image2 = document.querySelector('#image2');
+const image3 = document.querySelector('#image3');
+const textBox = document.querySelector('#text-box');
 
-input.addEventListener('click', () => {
-  console.log('checked', input.checked);
-});
+function darkMode() {
+  nav.style.backgroundColor = 'hsl(0 0% 0% / 80%)';
+  textBox.style.backgroundColor = 'hsl(0 0% 100% / 65%)';
+  themeText.textContent = 'Dark mode';
+  themeIcon.textContent = '🌙';
+  image1.src = 'img/undraw_proud_coder_dark.svg';
+  image2.src = 'img/undraw_feeling_proud_dark.svg';
+  image3.src = 'img/undraw_conceptual_idea_dark.svg';
+}
+
+function lightMode() {
+  nav.style.backgroundColor = 'hsl(0 0% 100% / 85%)';
+  textBox.style.backgroundColor = 'hsl(0 0% 0% / 45%)';
+  themeText.textContent = 'Light mode';
+  themeIcon.textContent = '☀️';
+  image1.src = 'img/undraw_proud_coder_light.svg';
+  image2.src = 'img/undraw_feeling_proud_light.svg';
+  image3.src = 'img/undraw_conceptual_idea_light.svg';
+}
+
+function switchTheme() {
+  if (input.checked) {
+    document.body.setAttribute('data-theme', 'dark');
+    darkMode();
+  } else {
+    document.body.setAttribute('data-theme', 'light');
+    lightMode();
+  }
+}
+
+input.addEventListener('click', switchTheme);
